610a31128d572d3ce3f28ae7ccd4429b1dd332a9
imyongchan <yongchan01045@naver.com>
2026-02-25 09:47:24 +0900

    YC_내담자 동의서

--- JINRO_PROJ/frontend/src/App.jsx
+++ JINRO_PROJ/frontend/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Agree from "./pages/student/agree";
+import Video from "./pages/student/video";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      {/* 동의서 페이지 */}
+      <Route path="/student/agree" element={<Agree />} />
+
+      {/* 영상 시청 페이지 */}
+      <Route path="/student/video" element={<Video />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
