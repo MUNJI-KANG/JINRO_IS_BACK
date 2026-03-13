755d99b56356a5fa7aa30500a36a9658cf52276d
imyongchan <yongchan01045@naver.com>
2026-03-13 11:24:45 +0900

    YC_run 서버

--- JINRO_PROJ/ai_server/run.py
+++ JINRO_PROJ/ai_server/run.py
@@ -8,4 +8,5 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 if __name__ == "__main__":
     # app 폴더 안의 main.py에 있는 app 객체를 실행합니다.
     # reload=True는 코드를 수정하면 서버가 자동으로 재시작되는 설정입니다.
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8001,reload=True )
+    uvicorn.run("app.main:app", host="54.66.250.193", port=9000,reload=True ) # 서버 동작 
+    # uvicorn.run("app.main:app", host="127.0.0.1", port=8001,reload=True ) # 로컬 
