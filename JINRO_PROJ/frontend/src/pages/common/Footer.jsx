51b7e79813e7fca830ecf0ac0b26bdf7c8d11df1
AHW <dksldnjs22@gmail.com>
2026-03-17 10:07:22 +0900

    HW - 학생 리스트 및 푸터 CSS

--- JINRO_PROJ/frontend/src/pages/common/Footer.jsx
+++ JINRO_PROJ/frontend/src/pages/common/Footer.jsx
@@ -4,8 +4,9 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>진로이즈백</p>
-        <p>02-6952-3610</p>
+        <div>진로이즈백</div>
+        <div>02-6952-3610</div>
+        <div>© 2026 JINRO IS BACK. All rights reserved.</div>
       </div>
     </footer>
   );
