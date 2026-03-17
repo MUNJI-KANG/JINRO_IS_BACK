2e8e81e1fbc00306771b64b2dc95388b28e5af7b
Dae_Kang <eorhkd7078@naver.com>
2026-03-17 15:40:34 +0900

    dk_수정

---
+++ JINRO_PROJ/frontend/src/pages/common/PageTransition.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../css/common_css/pageTransition.css";
+
+function PageTransition({ children }) {
+  return <div className="page-transition">{children}</div>;
+}
+
+export default PageTransition;
